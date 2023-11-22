13c2148a3153841ea232982dd16c8b4931561ffc
Vaibhavi Sharma <vaibhavisharma473@gmail.com>
2023-11-22 21:03:24 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,114 @@
+var crsr=document.querySelector("#cursor")
+var blur=document.querySelector("#cursor-blur")
+document.addEventListener("mousemove",function(dets){
+    crsr.style.left=dets.x+30+"px"
+    crsr.style.top=dets.y+"px"
+    blur.style.left=dets.x-250+"px"
+    blur.style.top=dets.y-250+"px"
+
+})
+var h4all=document.querySelectorAll("#nav h4")
+h4all.forEach(function(elem){
+    elem.addEventListener("mouseenter",function(){
+        crsr.style.scale =3
+        crsr.style.border="0.5px solid #fff"
+        crsr.style.backgroundColor="transparent"
+    }) 
+    elem.addEventListener("mouseleave",function(){
+        crsr.style.scale =1
+        crsr.style.border="0px solid #95c11e"
+        crsr.style.backgroundColor="#95c11e"
+    }) 
+})
+
+
+gsap.to("#nav",{
+    backgroundColor:"#000",
+    duration:0.5,
+    height:"110px",
+    scrollTrigger:{
+        trigger:"#nav",
+        scroller:"body",
+        start:"top -10%",
+        end:"top -11%",
+        scrub:1
+    }
+})
+gsap.to("#main",{
+    backgroundColor:"#000",
+    scrollTrigger:{
+        trigger:"#main",
+        scroller:"body",
+        start:"top -25%",
+        end:"top -70%",
+        scrub:2
+        
+
+    }
+})
+
+gsap.from("#about-us img,#about-us-in",{
+    y:50,
+    opacity:0,
+    duration:1,
+    stagger:0.4,
+    scrollTrigger:{
+        trigger:"#about-us",
+        scroller:"body",
+        start:"top 70%",
+        end:"top 65%",
+        scrub:1,
+    }
+
+})
+gsap.from(".card ",{
+    scale:0.8,
+    opacity:0,
+    duration:1,
+    scrollTrigger:{
+        trigger:".card",
+        scroller:"body",
+        start:"top 70%",
+        end:"top 55%",
+        scrub:1,
+    }
+
+})
+gsap.from("#colon1",{
+    y:-70,x:-70,
+    scrollTrigger:{
+        trigger:"#colon1",
+        scroller:"body",
+        start:"top 50%",
+        end:'top 47%',
+        scrub:2,
+
+
+    }
+})
+gsap.from("#colon2",{
+    y:70,x:70,
+    scrollTrigger:{
+        trigger:"#colon1",
+        scroller:"body",
+        start:"top 50%",
+        end:'top 47%',
+        scrub:2,
+
+
+    }
+})
+gsap.from("#page4 h1",{
+    y:50,
+    scrollTrigger:{
+        trigger:"#page4 h1",
+        scroller:"body",
+        start:"top 75%",
+        end:'top 70%',
+        scrub:2,
+
+
+    }
+
+
+})
